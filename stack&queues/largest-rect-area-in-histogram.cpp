5f53f9212f0f5390a638626551074222b9d6797b
Ranga-pavan <33192842+Ranga-pavan@users.noreply.github.com>
2021-05-29 19:43:00 +0530

    Create largest-rect-area-in-histogram.cpp

---
+++ stack&queues/largest-rect-area-in-histogram.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& h) {
+        int n=h.size();
+        stack<int> st;
+        int i = 0;
+        int maxarea=0,tp,area;
+            while(i<n){
+                if(st.empty() || h[st.top()]<=h[i]){                  //if coming val(6) is more than top add to stack hence rectangle formed
+                    st.push(i);                                       //h[st.top()](2) not <= 1
+                    i++;                                              // in 1 5 6 2
+                }
+                else{                                                 //(2 came)else pop(6,5) val upto which  rectangle forms
+                    tp=st.top();
+                    st.pop();
+                    area = h[tp]*(st.empty()? i:i-st.top()-1);
+                    if(maxarea<area)
+                    {
+                        maxarea = area;
+                    }
+                }
+            }
+        while(!st.empty()){
+            tp=st.top();
+            st.pop();
+            
+            area = h[tp]*(st.empty()? i:i-st.top()-1);          //if stack is empty it is the minimum it is equal to length and no s.top() also 
+            if(maxarea<area){                                   //i is next smaller element st.top() is first element of that rectangle
+                maxarea = area;
+            }
+        }
+        return maxarea;
+    }
+};
