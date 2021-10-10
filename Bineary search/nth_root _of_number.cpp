10d9fce676882569a2f14ae92ee104ca2677d3bd
Ranga-pavan <33192842+Ranga-pavan@users.noreply.github.com>
2021-10-10 21:31:18 +0530

    Create nth_root _of_number.cpp

---
+++ Bineary search/nth_root _of_number.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+double multiply(double number, int n) {
+    double ans = 1.0;
+    for(int i = 1;i<=n;i++) {
+        ans = ans * number;
+    }
+    return ans; 
+}
+
+double getNthRoot(int n, int m) {
+    double low = 1;
+    double high = m;
+    double eps = 1e-6; 
+    
+    while((high - low) > eps) {
+        double mid = (low + high) / 2.0; 
+        if(multiply(mid, n) < m) {
+            low = mid; 
+        }
+        else {
+            high = mid; 
+        }
+    }
+    
+    cout << low << " " << high << endl; 
+    
+    // just to check
+    cout << pow(m, (double)(1.0/(double)n)); 
+}
+int main() {
+	int n, m;
+	cin >> n >> m; 
+	getNthRoot(n, m); 
+	return 0;
+}
+/*striver
+nth root m
+so tc=N(logM*10^d)
+N for N times multiplication function-double multiply
+searching array is of size k then binary search is logk
+[1,1.1,.2,....,26.9,30] taking 1 decimal place   size M*10^d where d is decimal place*/
